799f0c6e943e6f00be0cfd90c640db1dd3f3aef8
yu <mail@firdaus.org>
2021-08-25 12:17:41 +0700

    Add error handling

--- app.js
+++ app.js
@@ -9,5 +9,16 @@ const indexRouter = require('./routes')
 
 app.use('/', indexRouter)
 
+//  error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
+    error: {
+      code: err.status || 500,
+      message: err.message,
+      errors: err.errors,
+    }
+  })
+})
 
 module.exports = app
