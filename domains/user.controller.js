2699d20e200ad43905f473d29bc1940851b05a4c
yu <mail@firdaus.org>
2021-08-25 15:25:55 +0700

    Add createUser method

--- domains/user.controller.js
+++ domains/user.controller.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+
 const User = require('./user.model')
 
 exports.listUsers = async (req, res, next) => {
@@ -17,6 +19,31 @@ exports.getUser = async (req, res, next) => {
     const { id } = req.params
 
     const user = await User.findById(id)
+
+    res.json({
+      data: user,
+    })
+
+  } catch (e) {
+    throw e
+  }
+}
+
+exports.createUser = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { username, name, is_admin, password } = req.body
+
+    // hash password
+    const hash = await bcrypt.hash(password, 12)
+
+    const user = new User({
+      username,
+      name,
+      is_admin,
+      password: hash,
+    })
+    await user.save()
 
     res.json({
       data: user,
