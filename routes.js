4800ad21e5cbb4acee2dabe34845f8bcaec015fc
yu <mail@firdaus.org>
2021-08-25 15:46:37 +0700

    Add DELETE /v1/users/:id route

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const userController = require('./domains/user.controller')
 
+router.delete('/v1/users/:id', (req, res, next) => userController.deleteUser(req, res, next))
 router.patch('/v1/users/:id', (req, res, next) => userController.updateUser(req, res, next))
 router.get('/v1/users/:id', (req, res, next) => userController.getUser(req, res, next))
 router.post('/v1/users', (req, res, next) => userController.createUser(req, res, next))
