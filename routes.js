cb105acfac1720e3d62272744172586827728692
yu <mail@firdaus.org>
2021-08-25 15:25:44 +0700

    Add POST /v1/users route

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const userController = require('./domains/user.controller')
 
 router.get('/v1/users/:id', (req, res, next) => userController.getUser(req, res, next))
+router.post('/v1/users', (req, res, next) => userController.createUser(req, res, next))
 router.get('/v1/users', (req, res, next) => userController.listUsers(req, res, next))
 
 module.exports = router
