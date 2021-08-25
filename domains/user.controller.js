5b65ea143c99d10c49ecfcb39db8aaac974427c2
yu <mail@firdaus.org>
2021-08-25 14:43:50 +0700

    Add getUser method

--- domains/user.controller.js
+++ domains/user.controller.js
@@ -11,3 +11,18 @@ exports.listUsers = async (req, res, next) => {
     throw e
   }
 }
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const { id } = req.params
+
+    const user = await User.findById(id)
+
+    res.json({
+      data: user,
+    })
+
+  } catch (e) {
+    throw e
+  }
+}
