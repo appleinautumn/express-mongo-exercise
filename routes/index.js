32d1baf37ec62cb45f069eb8189b8aaf9cdbb998
yu <mail@firdaus.org>
2023-07-03 23:42:54 +0700

    Refactor

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,39 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const adminController = require('../domains/admin.controller')
-const loginController = require('../domains/login.controller')
-const userController = require('../domains/user.controller')
-const { authenticated, isAdmin } = require('../middlewares/authenticated')
+const adminController = require('../domains/admin.controller');
+const loginController = require('../domains/login.controller');
+const userController = require('../domains/user.controller');
+const { authenticated, isAdmin } = require('../middlewares/authenticated');
 
 // admin only
-router.delete('/admin/v1/users/:id', authenticated, isAdmin, (req, res, next) => adminController.deleteUser(req, res, next))
-router.patch('/admin/v1/users/:id', authenticated, isAdmin, (req, res, next) => adminController.updateUser(req, res, next))
-router.get('/admin/v1/users/:id', authenticated, isAdmin, (req, res, next) => adminController.getUser(req, res, next))
-router.post('/admin/v1/users', authenticated, isAdmin, (req, res, next) => adminController.createUser(req, res, next))
-router.get('/admin/v1/users', authenticated, isAdmin, (req, res, next) => adminController.listUsers(req, res, next))
+router.delete('/admin/v1/users/:id', authenticated, isAdmin, (req, res, next) =>
+  adminController.deleteUser(req, res, next)
+);
+router.patch('/admin/v1/users/:id', authenticated, isAdmin, (req, res, next) =>
+  adminController.updateUser(req, res, next)
+);
+router.get('/admin/v1/users/:id', authenticated, isAdmin, (req, res, next) =>
+  adminController.getUser(req, res, next)
+);
+router.post('/admin/v1/users', authenticated, isAdmin, (req, res, next) =>
+  adminController.createUser(req, res, next)
+);
+router.get('/admin/v1/users', authenticated, isAdmin, (req, res, next) =>
+  adminController.listUsers(req, res, next)
+);
 
 // authenticated users only
-router.get('/v1/profile', authenticated, (req, res, next) => userController.getProfile(req, res, next))
+router.get('/v1/profile', authenticated, (req, res, next) =>
+  userController.getProfile(req, res, next)
+);
 
 // no authentication needed
-router.post('/v1/refresh-token', (req, res, next) => loginController.refreshToken(req, res, next))
-router.post('/v1/logins', (req, res, next) => loginController.login(req, res, next))
+router.post('/v1/refresh-token', (req, res, next) =>
+  loginController.refreshToken(req, res, next)
+);
+router.post('/v1/login', (req, res, next) =>
+  loginController.login(req, res, next)
+);
 
-module.exports = router
+module.exports = router;
