e65285969854809091ae609e147abeed260d5442
Firdaus Adi Putra <mail@firdaus.org>
2025-03-26 14:42:32 +0700

    Skip db in test env

--- app.js
+++ app.js
@@ -6,25 +6,37 @@ const createError = require('http-errors');
 
 const router = require('./routes');
 
-//  setup database connection
-require('./db');
+// Skip database connection in test environment
+if (process.env.NODE_ENV !== 'test') {
+  require('./db');
+}
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', cors());
+app.use(cors());
+app.use(express.json());
+
+// API Routes
 app.use('/api', router);
 
-//  error handler
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    error: {
-      code: err.status || 500,
-      message: err.message,
-      errors: err.errors,
-    },
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404, 'Resource Not Found'));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV === 'development' ? err : {};
+
+  // render the error message
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    code: err.code,
+    name: err.name,
+    message: err.message,
+    data: err.data,
   });
 });
 
